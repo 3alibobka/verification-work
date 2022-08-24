20896bb78fe8acbda44f34dbc53f1bcf70f1bd9f
Ivan <3alib0bka@mail.ru>
2022-08-24 10:17:31 +0300

    code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿string[] array =
+{
+    "12345",
+    "32",
+    "Минск",
+    "5",
+    "мяч",
+    "Беларусь"
+};
+
+var result = new string[array.Length];
+var Size = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[Size] = value;
+        Size++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, Size));
